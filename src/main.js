e39a0111eed4c7b4d43358e10d7ee0d56967ac18
d0j1a_1701 <dajia1701@gmail.com>
2024-02-18 15:25:21 +0800

    能用了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // 运行在 Electron 主进程 下的插件入口
 
-
-
 // 创建窗口时触发
-module.exports.onBrowserWindowCreated = window => {
-    // window 为 Electron 的 BrowserWindow 实例
-}
+module.exports.onBrowserWindowCreated = (window) => {
+	// window 为 Electron 的 BrowserWindow 实例
+	window.once('show', () => {
+		window.setBackgroundColor('rgba(0, 0, 0, 0)');
+		window.setBackgroundMaterial('mica');
+	});
+};
